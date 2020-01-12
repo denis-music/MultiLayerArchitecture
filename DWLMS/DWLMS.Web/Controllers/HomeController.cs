38c649a37d44ca01224592dc7df95a8de207f038
denis <denis@edu.fit.ba>
2020-01-12 18:39:12 +0100

    Init

---
+++ DWLMS/DWLMS.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DWLMS.Web.Models;
+using DLWMS.Services;
+using DLWMS.Core;
+
+namespace DWLMS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+         ukoliko želite rad sa bazom podataka, prije pokretanja aplikacije 
+         potrebno je da izvršite migracijen a DLWMS.Repository projektu             
+         */
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStudentService _studentService;
+
+        public HomeController(ILogger<HomeController> logger, IStudentService studentService)
+        {
+            _logger = logger;
+            _studentService = studentService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Student> studenti = _studentService.GetStudents().ToList();
+            return View(studenti);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DWLMS/DWLMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DWLMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
